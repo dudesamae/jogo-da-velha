d82e05c4cfe9b9465f659085c2ceede3dd753374
madu <106332004+dudesamae@users.noreply.github.com>
2023-11-24 10:41:50 -0300

    jogo corrigido 2

--- jogo.js
+++ jogo.js
@@ -1,3 +1,16 @@
+// Seleciona todos os elementos com a classe "celula" e os armazena em uma lista.
+const celulas = document.querySelectorAll(".celula");
+
+// Inicializa a variável checarTurno como verdadeira.
+let checarTurno = true;
+
+// Inicializa a variável turno, que será usado para alternar entre "X" e "O".
+let turno;
+
+// Define constantes para representar os jogadores "X" e "O".
+const JOGADOR_X = "X";
+const JOGADOR_O = "O";
+
 // Inicializa a variável jogoAcabou como falso, indicando que o jogo está em andamento.
 let jogoAcabou = false;
 
